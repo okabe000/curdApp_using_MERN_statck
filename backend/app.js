00a3a73397f48a38cf5659a1b801a266340ae29e
okabe <azozf55@gmail.com>
2024-01-21 01:59:46 +0300

    production build working.

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,26 @@
 const express = require('express');
+const fs = require('fs'); // File system module, can be removed if not used elsewhere
 const cors = require('cors');
+
 const app = express();
-const logger = require('./src/utils/logger');
 const loggerMiddleware = require('./src/middleware/loggerMiddleware');
 const userRoutes = require('./src/api/routes/users');
 const itemRoutes = require('./src/api/routes/items');
-
-// Import the database connection function
 const connectDatabase = require('./src/config/db');
+const crypto = require('crypto');
 
 // Other setup like middleware, routes, etc.
 app.use(express.json());
 app.use(cors());
 app.use(loggerMiddleware);
-const crypto = require('crypto');
 
-const generateSecret = () => {
-    return crypto.randomBytes(64).toString('hex');
-};
-
+const generateSecret = () => crypto.randomBytes(64).toString('hex');
 const jwtSecret = generateSecret();
 console.log('JWT Secret:', jwtSecret);
 
-// Use the user and item routes
 app.use('/api/users', userRoutes);
 app.use('/api/items', itemRoutes);
 
-// Other routes and error handling
-
-// Database connection and server startup
 const startServer = async () => {
   const maxRetries = 5;
   const port = process.env.PORT || 3000;
@@ -37,17 +29,20 @@ const startServer = async () => {
     try {
       await connectDatabase();
       console.log('Database connection successful');
-      app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+      // Starting an HTTP server
+      app.listen(port, () => {
+        console.log(`HTTP Server is running on port ${port}`);
+      });
       return;
     } catch (error) {
       console.error(`Failed to connect to the database (attempt ${i + 1}):`, error);
-      // Wait for 5 seconds before retrying
       await new Promise(resolve => setTimeout(resolve, 5000));
     }
   }
 
   console.error('Could not connect to the database after several attempts. Exiting...');
-  process.exit(1); // Exit the process with failure code
+  process.exit(1);
 };
 
 startServer();
